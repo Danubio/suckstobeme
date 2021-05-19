1e3aa1eacbaa06ff2cf085b75c05865911e22b3e
Tiago Carvalho <E855@itsector.pt>
2021-05-19 18:02:06 +0100

    CSS and JS

---
+++ app.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const userGrid = document.querySelector('.grid-user')
+    const computerGrid = document.querySelector('.grid-computer')
+    const displayGrid = document.querySelector('.grid-display')
+
+    const ships = document.querySelectorAll('.ship')
+    const destroyer = document.querySelector('.destroyer-container')
+    const destroyer = document.querySelector('.submarine-container')
+    const destroyer = document.querySelector('.destroyer-container')
+    const destroyer = document.querySelector('.destroyer-container')
+    const destroyer = document.querySelector('.destroyer-container')
+    const destroyer = document.querySelector('.destroyer-container')
+})
