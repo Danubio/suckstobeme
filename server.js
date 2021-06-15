638a474531cb95bafbdaddbe87e0fca50c775696
Tiago Carvalho <tiagocarvalho56@hotmail.com>
2021-06-16 00:40:16 +0100

    Add sorce code from gril and started node code

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const path = require('path')
+const http = require('http')
+const PORT = process.env.PORT || 3000
+const socketio = require('socket.io')
+const { Socket } = require('dgram')
+
+const app = express();
+const server  = http.createServer(app)
+const io = socketio(server)
+
+//set static folder 
+app.use(express.static(path.join(__dirname, "public")))
+
+//start server
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+//handle a socket connection request from web client
+const connections = [null, null]
+
+io.on('connection', socket =>{
+    //console.log('new connection')
+
+    //find available player
+    let playerIndex = -1;
+    for(const i in connections) {
+        if(connections[i] === null)
+        {
+            playerIndex = i
+            break
+        }
+    }
+
+    
+
+    //tell the connection client what player number they are
+    socket.emit('player-number', playerIndex)
+
+    console.log(`Player ${playerIndex} has connected`)
+
+    //ignore player 3
+    if(playerIndex === -1) return
+})
